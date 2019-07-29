90599a938021297ddc13758897161e5233489356
Juan Manuel Aguero <juanma.aguero@gmail.com>
2019-07-29 18:00:14 -0300

    Add admin controller to module.

--- src/social-explorer.module.ts
+++ src/social-explorer.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SocialExplorerService } from './social-explorer.service';
+import {AdminSocialController} from './admin-social.controller';
 
 @Module({
   providers: [SocialExplorerService],
   exports: [SocialExplorerService],
+  controllers: [AdminSocialController],
 })
 export class SocialExplorerModule {}

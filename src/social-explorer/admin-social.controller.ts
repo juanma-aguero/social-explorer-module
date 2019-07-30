6fa838967ecb0364d0a473e3686c3a0c66306e60
Juan Manuel Aguero <juanma.aguero@gmail>
2019-07-30 02:29:50 -0300

    Add twitter service

--- src/social-explorer/admin-social.controller.ts
+++ src/social-explorer/admin-social.controller.ts
@@ -1,15 +1,28 @@
 import {Controller, Get, HttpStatus, Response, UseGuards} from '@nestjs/common';
 import {ApiUseTags} from '@nestjs/swagger';
+import {SocialExplorerService} from "./social-explorer.service";
 
 @ApiUseTags('social-explorer')
 @Controller('admin/social-explorer')
 export class AdminSocialController {
+  constructor(private socialExp: SocialExplorerService) {
+  }
 
   @Get('defaults')
   async getDefaults(@Response() res: any): Promise<any> {
 
     return res.status(HttpStatus.OK).json({
       sources: 1,
+    });
+  }
+
+  @Get('twitter')
+  async getTwitter(@Response() res: any): Promise<any> {
+
+    await this.socialExp.getPublications();
+
+    return res.status(HttpStatus.OK).json({
+      'message': 'ok',
     });
   }
 }
--- src/social-explorer/social-explorer.service.ts
+++ src/social-explorer/social-explorer.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {Twitter} from 'twitter';
 
 @Injectable()
-export class SocialExplorerService {}
+export class SocialExplorerService {
+  private twitterClient;
+
+  constructor() {
+    this.twitterClient = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      bearer_token: process.env.TWITTER_BEARER_TOKEN
+    });
+  }
+
+  async getPublications() {
+
+    return await this.twitterClient.get('search/tweets', {q: '#ios #swift'}, function (error, tweets, response) {
+      tweets.statuses.forEach(function (tweet) {
+        console.log("tweet: " + tweet.text)
+      });
+    });
+
+  }
+
+}
